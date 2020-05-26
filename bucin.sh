64416e7c3f4427efd4fbbf5c2192fdf1346cb8dd
suzuxploit <64567892+suzuxploit@users.noreply.github.com>
2020-05-26 20:38:19 +0700

    Add files via upload

---
+++ bucin.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+#color(bold)
+red = '\e[1;31m'
+green = '\e[1;32m'
+yellow = '\e[1;33m'
+blue = '\e[1;34m'
+magenta = '\e[1;35m'
+cyan = '\e[1;36m'
+white = '\e[1;37m'
+
+#banner
+clear
+figlet Tools Bucin
+echo "+---------------------------------------------->
+echo "+---------------------------------------------->
+echo "+ Author : Suzu         | Galau, Bucin, dll:v  >
+echo "+ Team   : MonchaXploit | Curhat sini aj lur >
+echo "+---------------------------------------------->
+echo "+------>>>https://suzunix.blogspot.com/<<<----->
+echo ""
+echo ""
+echo "Pilihan Tools :"
+echo ""
+echo "[1] Bucin"
+echo "[2] Galau"
+echo "[3] Obat Galau"
+echo "[4] Obat Bucin"
+echo ""
+read -p  $'Select@Number: '\e[1; 33m pilih
+  if [$pilih = "1"]
+  then
+  clear
+  echo "Kamu sayang dia ?"
+  clear
+  sleep 1
+  echo "Percuma kamu sayang dia tetapi dia sayang>
+      clear
+      sleep 1
+      echo "Kamu cuma mengejar sesuatu yang tidak pas >
+  clear
+  sleep 1
+  echo "Terlalu lama memperjuangkan nya tetapi ti>
+      clear
+      sleep 1
+      echo "Dia bukanya tidak peka, dia memang tidak >
+  clear
+  sleep 1
+  echo "Yang sabar ya bro, kita senasib :)"
+  fi
+
+  if [$pilih = "2"]
+  then
+  clear
+  echo "[+] Aku sayang sama kamu"
+  sleep 4
+  echo "[+] Tapi kenapa kamu pilih kasih sama>
+      sleep 4
+      echo "[+] Apa bedanya aku sama dia ?"
+      sleep 4
+      echo "[+] Sampai kamu lebih memilih dia !"
+      sleep 4
+      echo "[+] Aku sudah beri kamu kesempatan"
+      sleep 4
+      echo "[+] Eh malah kamu mengecewakan aku !!"
+fi
+
+if [ $pilih = "3" ]
+then
+     clear
+     echo "█████████"
+     echo "█▄█████▄█"
+     echo "█▼▼▼▼▼"
+     echo "█"
+     echo "█▲▲▲▲▲"
+     echo "█████████"
+     echo "██     ██"
+     echo ""
+     echo "[*] Berikut kumpulan sholawat"
+     echo "[*] Yang dapat menghilangkan rasa"
+     echo "[*] Galau + Gundah + Bosan + dll"
+     echo ""
+     echo "Daftar sholawat :"
+     echo ""
+     echo "[1] Robbi kholaq - Majelis Rasulullah >
+  echo "[2] Ya tarim - Majelis Nurul Musthofa"
+  echo "[3] Ya hababah Khadijah"
+  echo "[4] Syailillah Tawasul - Nurul Musthof>
+     echo ""
+     echo "Semoga Bermanfaat Sholawat Berikut :)"
+fi
